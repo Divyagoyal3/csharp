64a17ceac71a2b46f016521f9ab54a8fc52106ca
Divyagoyal3 <goyaldivya631@gmail.com>
2022-07-22 17:35:58 +0530

    -day1

---
+++ console1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class second
+{
+    public static  void Main ( String[] args)
+    {
+      first f1 = new first();
+      f1.firstfun1();  
+      Console.WriteLine("Hello, World!");
+    }
+
+
+
+}
+
---
+++ console1/first.cs
@@ -0,0 +1,10 @@
+using System ;
+class first
+{
+   public void firstfun1()
+   {
+       int a =10, b=20;
+       int c=a+b;
+       Console.WriteLine(c);
+   }
+}
