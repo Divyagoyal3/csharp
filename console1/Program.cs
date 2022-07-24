810d2a421919bed7c9570369fce86cdb676cb55f
Divyagoyal3 <goyaldivya631@gmail.com>
2022-07-24 21:47:55 +0530

    first.commit

--- console1/Program.cs
+++ console1/Program.cs
@@ -6,7 +6,26 @@ class second
     {
       first f1 = new first();
       f1.firstfun1();  
+      
+      Console.Title="First Site";
+      Console.ForegroundColor=ConsoleColor.Green;
+      Console.WindowHeight=30;
+
+
       Console.WriteLine("Hello, World!");
+      
+      
+      // Type your username and press enter
+Console.WriteLine("Enter username:");
+
+// Create a string variable and get user input from the keyboard and store it in the variable
+string userName = Console.ReadLine();
+
+// Print the value of the variable (userName), which will display the input value
+Console.WriteLine("Username is: " + userName);
+
+      Console.WriteLine("Day 2 Learning with C#");
+      Console.ReadLine();
     }
 
 
